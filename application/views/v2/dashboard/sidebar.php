355e24dd42c8afda956a8af6c25a9c92bbcbe373
aldinoesphere <aldino.said@esphere.id>
2021-08-24 11:41:26 +0700

    Migrate module master data sidebar to v2

--- application/views/v2/dashboard/sidebar.php
+++ application/views/v2/dashboard/sidebar.php
@@ -3,7 +3,7 @@
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
             <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
-            <span class="brand-text font-weight-light">AdminLTE 3</span>
+            <span class="brand-text font-weight-light">TOKO GROSIR LUSINAN</span>
             </a>
             <!-- Sidebar -->
             <div class="sidebar">
@@ -13,7 +13,7 @@
                      <img src="<?php echo base_url('assets/v2/dist/img/user2-160x160.jpg'); ?>" class="img-circle elevation-2" alt="User Image">
                   </div>
                   <div class="info">
-                     <a href="#" class="d-block">Alexander Pierce</a>
+                     <a href="#" class="d-block"><?php echo ucfirst($this->session->userdata('username')); ?></a>
                   </div>
                </div>
                <!-- SidebarSearch Form -->
@@ -30,10 +30,35 @@
                <!-- Sidebar Menu -->
                <nav class="mt-2">
                   <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                     <?php
+                        $segment['master_data'] = [
+                           'barang',
+                           'kategori',
+                           'jenis',
+                           'merek',
+                           'satuan',
+                           'suplier',
+                           'customer'
+                        ];
+                        $segment['transaksi'] = [
+                           'daftar_transaksi',
+                           'penjualan',
+                           'pembelian'
+                        ];
+                        $segment['laporan'] = [
+                           'penjualan',
+                           'pembelian'
+                        ];
+                        $segment['pengaturan'] = [
+                           'pengguna',
+                           'factory_reset',
+                           'test_print'
+                        ];
+                     ?>
                      <!-- Add icons to the links using the .nav-icon class
                         with font-awesome or any other icon font library -->
-                     <li class="nav-item menu-open">
-                        <a href="#" class="nav-link active">
+                     <li class="nav-item <?php if (in_array($this->uri->segment(1), $segment['master_data'])) {echo 'menu-open';} ?>">
+                        <a href="#" class="nav-link <?php if (in_array($this->uri->segment(1), $segment['master_data'])) {echo 'active';} ?>">
                            <i class="nav-icon fas fa-cubes"></i>
                            <p>
                               MASTER DATA
@@ -42,45 +67,97 @@
                         </a>
                         <ul class="nav nav-treeview">
                            <li class="nav-item">
-                              <a href="./index.html" class="nav-link">
+                              <a href="<?php echo base_url('barang'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'barang') { echo 'active';} ?>">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>BARANG</p>
                               </a>
                            </li>
                            <li class="nav-item">
-                              <a href="./index2.html" class="nav-link">
+                              <a href="<?php echo base_url('kategori'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'kategori') { echo 'active';} ?>">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>KATEGORI</p>
                               </a>
                            </li>
                            <li class="nav-item">
-                              <a href="./index3.html" class="nav-link">
+                              <a href="<?php echo base_url('jenis'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'jenis') { echo 'active';} ?>">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>JENIS</p>
                               </a>
                            </li>
                            <li class="nav-item">
-                              <a href="./index3.html" class="nav-link">
+                              <a href="<?php echo base_url('merek'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'merek') { echo 'active';} ?>">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>MEREK</p>
                               </a>
                            </li>
                            <li class="nav-item">
-                              <a href="./index3.html" class="nav-link">
+                              <a href="<?php echo base_url('satuan'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'satuan') { echo 'active';} ?>">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>SATUAN</p>
                               </a>
                            </li>
                            <li class="nav-item">
-                              <a href="./index3.html" class="nav-link">
+                              <a href="<?php echo base_url('suplier'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'suplier') { echo 'active';} ?>">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>SUPLIER</p>
                               </a>
                            </li>
                            <li class="nav-item">
-                              <a href="./index3.html" class="nav-link active">
+                              <a href="<?php echo base_url('customer'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'customer') { echo 'active';} ?>">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>CUSTOMER</p>
+                              </a>
+                           </li>
+                        </ul>
+                     </li>
+                     <li class="nav-item">
+                        <a href="#" class="nav-link <?php if (in_array($this->uri->segment(1), $segment['transaksi'])) {echo 'active';} ?>">
+                           <i class="nav-icon fas fa-exchange-alt"></i>
+                           <p>
+                              TRANSAKSI
+                              <i class="right fas fa-angle-left"></i>
+                           </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                              <a href="<?php echo base_url('daftar_transaksi'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'daftar_transaksi') { echo 'active';} ?>">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>DAFTAR TRANSAKSI</p>
+                              </a>
+                           </li>
+                           <li class="nav-item">
+                              <a href="<?php echo base_url('penjualan'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'penjualan') { echo 'active';} ?>">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>PENJUALAN</p>
+                              </a>
+                           </li>
+                           <li class="nav-item">
+                              <a href="<?php echo base_url('pembelian'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'pembelian') { echo 'active';} ?>">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>PEMBELIAN</p>
+                              </a>
+                           </li>
+                        </ul>
+                     </li>
+                     <li class="nav-item">
+                        <a href="#" class="nav-link <?php if (in_array($this->uri->segment(1), $segment['laporan'])) {echo 'active';} ?>">
+                           <i class="nav-icon fas fa-file"></i>
+                           <p>
+                              LAPORAN
+                              <i class="right fas fa-angle-left"></i>
+                           </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                           <li class="nav-item">
+                              <a href="<?php echo base_url('pembelian'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'pembelian') { echo 'active';} ?>">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>PENJUALAN</p>
+                              </a>
+                           </li>
+                           <li class="nav-item">
+                              <a href="<?php echo base_url('pembelian'); ?>" class="nav-link <?php if ($this->uri->segment(1) == 'pembelian') { echo 'active';} ?>">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>PEMBELIAN</p>
                               </a>
                            </li>
                         </ul>
